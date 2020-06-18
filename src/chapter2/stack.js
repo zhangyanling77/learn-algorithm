9d5fe84884d36d2b61844848906a0e04c384cfc2
zhangyanling <18783226594@163.com>
2020-06-18 19:58:02 +0800

    refactor: stack add

--- src/chapter2/stack.js
+++ src/chapter2/stack.js
@@ -9,13 +9,25 @@ class Stack {
     // 栈顶位置
     this.top = 0;
   }
-
+  // 入栈
   push(element) {
     this.data[this.top++] = element
   }
-
+  // 出栈
   pop() {
     return this.data[--this.top]
+  }
+  // 返回栈顶元素
+  peek() {
+    return this.data[this.top - 1]
+  }
+  // 返回栈的长度
+  size() {
+    return this.top
+  }
+  
+  clear() {
+    this.top = 0
   }
 
   output() {
@@ -32,6 +44,14 @@ stack.push(2)
 stack.push(3)
 stack.push(4)
 
-stack.output() // 1 2 3 4
+// console.log(stack.peek()) // 4
+
+// stack.output() // 1 2 3 4
 
 console.log(stack.pop()) // 4
+
+console.log(stack.size()) // 3
+
+stack.clear()
+
+console.log(stack.size()) // 0
