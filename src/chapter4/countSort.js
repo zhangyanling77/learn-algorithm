d6e142f4e8e532b8208e8f56c1a7e4e29693628e
zhangyanling77 <18783226594@163.com>
2020-07-06 10:20:51 +0800

    feat: 优化 count sort

--- src/chapter4/countSort.js
+++ src/chapter4/countSort.js
@@ -35,7 +35,46 @@ function output(array) {
   }
 }
 
-const array = [4, 4, 6, 5, 3, 2, 8, 1, 7, 5, 6, 0, 10];
-let sortArray = countSort(array);
+// const array = [4, 4, 6, 5, 3, 2, 8, 1, 7, 5, 6, 0, 10];
+// const sortArray = countSort(array);
 
-output(sortArray)
+// output(sortArray)
+
+
+// 优化版 计数排序
+function countSort2(array) {
+  // 1.得到数列的最大值和最小值，并计算出差值d
+  let max = array[0];
+  let min = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i]
+    }
+    if (array[i] < min) {
+      min = array[i]
+    }
+  }
+  let d = max - min;
+  // 2.创建统计数组并统计对应元素的个数
+  let countArray = new Array(d + 1);
+  countArray.fill(0);
+  for (let i = 0; i < array.length; i++) {
+    countArray[array[i] - min]++;
+  }
+  // 3.统计数组做变形，后面的元素等于前面的元素之和
+  for (let i = 1; i < countArray.length; i++) {
+    countArray[i] += countArray[i - 1];
+  }
+  // 4.倒序遍历原始数列，从统计数组找到正确位置，输出到结果数组
+  let sortedArray = new Array(array.length);
+  for (let i = array.length - 1; i >= 0; i--) {
+    sortedArray[countArray[array[i] - min] - 1] = array[i];
+    countArray[array[i] - min]--;
+  }
+  return sortedArray;
+}
+
+const array2 = [95, 94, 91, 98, 99, 90, 99, 93, 91, 92];
+const sortArray2 = countSort2(array2);
+
+output(sortArray2) // 90 91 91 92 93 94 95 98 99 99
